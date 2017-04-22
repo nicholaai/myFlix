23dabfc71b6fb7cf9a05edb1131920ace9b0231b
Nicholas Pappagalo <npappagalo@gmail.com>
2017-04-22 11:03:19 -0700

    enable redux dev tools

--- js/store.js
+++ js/store.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+// if we are in browser and redux dev tools available, run them.
+// dont want for node aka pre-rendering on node
+const store = createStore(rootReducer, compose(
+  typeof window === 'object' && typeof window.devToolsExtension !== 'undefined'
+  ? window.devToolsExtension() : (f) => f
+));
 
 export default store;
