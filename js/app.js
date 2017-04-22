48385be7dc41371cd7b1e0a4c4f0048739d8ced9
Nicholas Pappagalo <npappagalo@gmail.com>
2017-04-22 16:27:31 -0700

    optimize with aync routing, code splitting, and preact

--- js/app.js
+++ js/app.js
@@ -2,24 +2,30 @@ import React from 'react';
 import { Match } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import Landing from './Landing';
-import Search from './Search';
-import Details from './Details';
+import AsyncRoute from './AsyncRoute';
 import preload from '../public/data.json';
+// node dont fail when import is called.
+if (global) {
+  global.System = { import () {} };
+}
 
 const App = () => {
   return (
     <Provider store={store}>
       <div className='app'>
-        <Match exactly pattern='/' component={Landing} />
+        <Match
+          exactly
+          pattern='/'
+          component={(props) => <AsyncRoute props={props} loadingPromise={System.import('./Landing')} />}
+        />
         <Match
           pattern='/search'
-          component={(props) => <Search shows={preload.shows} {...props} />} />
+          component={(props) => <AsyncRoute props={Object.assign({shows: preload.shows}, props)} loadingPromise={System.import('./Search')} />} />
         <Match
           pattern='/details/:id'
           component={(props) => {
             const show = preload.shows.filter((show) => props.params.id === show.imdbID);
-            return <Details show={show[0]} {...props} />;
+            return <AsyncRoute props={Object.assign({show: show[0]}, props)} loadingPromise={System.import('./Details')} />;
           }}
         />
       </div>
