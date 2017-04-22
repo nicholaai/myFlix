cf0dd2e7b4c41410ecfa5eb7ce77cddf6a9e687e
Nicholas Pappagalo <npappagalo@gmail.com>
2017-04-22 14:15:27 -0700

    add async redux for imdb ratings via omdb call

--- js/actionCreators.js
+++ js/actionCreators.js
@@ -1,5 +1,21 @@
-import { SET_SEARCH_TERM } from './actions';
+import { SET_SEARCH_TERM, ADD_OMDB_DATA } from './actions';
+import axios from 'axios';
 
 export function setSearchTerm (searchTerm) {
   return { type: SET_SEARCH_TERM, searchTerm };
+};
+
+export function addOMDBData (imdbID, omdbData) {
+  return { type: ADD_OMDB_DATA, imdbID, omdbData };
+};
+
+// thunk creators return a function - user calls getOMDBDetails redux calls callback
+export function getOMDBDetails (imdbID) {
+  return function (dispatch, getState) {
+    axios.get(`http://www.omdbapi.com/?i=${imdbID}`)
+      .then((response) => {
+        dispatch(addOMDBData(imdbID, response.data));
+      })
+      .catch((error) => console.error('axios error', error));
+  };
 };
--- js/store.js
+++ js/store.js
@@ -1,9 +1,11 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 // if we are in browser and redux dev tools available, run them.
 // dont want for node aka pre-rendering on node
 const store = createStore(rootReducer, compose(
+  applyMiddleware(thunk),
   typeof window === 'object' && typeof window.devToolsExtension !== 'undefined'
   ? window.devToolsExtension() : (f) => f
 ));
